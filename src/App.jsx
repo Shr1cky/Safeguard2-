acc22c7a7c7de890f43f0909e9c4a0af91ad2a77
Shr1cky <Viggolakner@gmail.com>
2026-01-13 20:33:40 +0100

    Fix GitHub Pages routing: add 404.html redirect and configure base path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Layout from './components/Layout'
 function App() {
   return (
     <ProfileProvider>
-      <Router>
+      <Router basename="/Safeguard2-">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
